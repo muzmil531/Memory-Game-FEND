69e4222518fda4202caa020eebef448cd2255e63
muzmil531 <muzu.muzmil@gmail.com>
2019-06-18 15:44:48 -0700

    Memory Game FEND

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Memory-Game-FEND
+# Memory Game Project
+
+## Table of Contents
+
+-   [Instructions](#instructions)
+-   [Contributing](#contributing)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+
+## For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+
+* * *
+
+## I concluded this project by using Below Steps.
+
+### Cloning of the project
+
+-   I had cloned the project file after reading the instructions, by using the GitHub link about the skeleton project which is provided by **Udacity** in rubic structure I Cloned the file.
+-   After Cloning the file in the system from GitHub, I observed the files as listed below.
+    1.  `css/app.css` - contains styles to the project.
+    2.  `img/geometry2.png`- contains images that used as background image.
+    3.  `js/app.js` - JavaScript file that they provided.
+    4.  `index.html` - HTML file of the project.
+    5.  `README.md` - READMEfile for the project.
+
+### Modification of Code To get game
+1.  Added `reload()` function to refresh the page using script.
+2.  Intialised  a function to shuffle all the cards,
+3.  Added a  click **EventListener**  to each card. when clicking any card `showCard()` function triggers.
+4.  Added a `startTimer()` function, so that when an user starts the game(clicking first card), the time should automatically started and after end of game it should automatically stopped using `clearInterval()` method.
+5.  Using if condition, we tested if any two cards gets open, then count of moves will be increased.
+6.  Added a function to the program called `showCard()` so that after user clicked on it, it get displayed, while click on the card, timer get started  automatically.
+7.  Included a class (`disable`) to disable the card, so that if two  card matches, these are getting disabled automatically.
+8.  Included a method so that if previous card does not match with currentCard then both cards should be closed.
+9.  Instialied a function for star counter, so that if the moves are between 12 to 18, then star count will be 2.If moves get exceeded by 18, then star count should be 1, else star count will be 3.
+10. Intialised timer display so that the user gets to know the time.
+11. Finally Intialised sweetalert to show the congratulatory message along with time to complete the game, moves taken to complete the game and stars that the user earned.
+
+### OUTPUT OF THE Game
+
+1.  If all cards get matched, then a popup alert will raise with report of the game.
+
+### MEDIA queries
+
+1. This game is compactable in Desktop mode, Mobile mode, Tablet Mode.
---
+++ js/app.js
@@ -0,0 +1,146 @@
+"use strict";
+var parent1 = document.getElementById('deck');
+/*
+ * Create a list that holds all of your cards
+ */
+// Intialising or declaring all the variables required
+var child1List = Array.prototype.slice.call(document.querySelectorAll(".card"));
+let tStatus = 0;
+var time;
+var move = 0;
+var tArea = document.getElementById('time');
+var cardStorage = [];
+var moveSection = document.getElementById('moves');
+var hrs;
+var min;
+var sec;
+var starSection = [...document.getElementsByClassName("fa-star")];
+var starCount = 3;
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+// Intialisings shuffling the cards
+window.onload = shuffleGame();
+// shuffleGame Cards
+function shuffleGame() {
+	var shuffledCards = shuffle(child1List);
+	var k = 0;
+	while (k < shuffledCards.length) {
+		console.log(shuffledCards[k]);
+		parent1.append(shuffledCards[k]);
+		k++;
+	}
+}
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+	var currentIndex = array.length,
+		temporaryValue, randomIndex;
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
+//
+// on click showing card
+for (var s in child1List) {
+	child1List[s].addEventListener("click", showCard);
+}
+
+function showCard() {
+	if (tStatus == 0) {
+		startTimer(); /*time start function*/
+		tStatus = tStatus + 1;
+	}
+	this.classList.add("card");
+	this.classList.add("open");
+	this.classList.add("show");
+	/* card disablemethod */
+	this.classList.add("disable");
+	cardStorage.push(this);
+	if (cardStorage.length == 2) {
+		// console.log.(cardStorage[0].children[0].classList.item(1));
+		move = move + 1;
+		srating();
+    // displaying moves count in html
+		moveSection.innerHTML = move;
+		if (cardStorage[0].children[0].classList.item(1) == cardStorage[1].children[0].classList.item(1)) {
+			console.log("Matched");
+			cardStorage[0].classList.add("match", "disable");
+			cardStorage[1].classList.add("match", "disable");
+			if (matchCards.length == 16) {
+				clearInterval(time);
+				// Alert Message on Game Completion
+				Swal.fire({
+					title: 'Congratulations',
+					html: 'You Have earned <strong style="color:#ff9f33; text-shadow: 3px 3px 3px #000">' + starCount + '<i class ="fa fa-star"></i></strong> <br> and You Comleted this game with the time of <br>' + hrs + " :: " + min + " :: " + sec,
+					animation: false,
+					confirmButtonText: '<i class="fa fa-thumbs-up"></i> Restart',
+					customClass: {
+						popup: 'animated tada'
+					}
+				}).then(() => {
+					document.location.reload(); /* page reload method */
+				});
+			}
+			cardStorage = [];
+		} else {
+			console.log("un matched");
+			cardStorage[0].classList.add("unmatch");
+			cardStorage[1].classList.add("unmatch");
+			console.log(cardStorage[0]);
+			cardStorage.map((card) => {
+				setTimeout(() => {
+					card.classList.remove("unmatch", "open", "show", "disable");
+				}, 500)
+			})
+			cardStorage = [];
+		}
+	}
+}
+var matchCards = document.getElementsByClassName("match");
+// Timer functionality
+function startTimer() {
+	sec = 0;
+	min = 0;
+	hrs = 0;
+	time = setInterval(() => {
+		sec = sec + 1;
+		if (sec == 59) {
+			min = min + 1;
+			sec = 0;
+		}
+		if (min == 60) {
+			hrs = hrs + 1;
+			min = 0;
+		}
+		tArea.innerHTML = hrs + " :: " + min + " :: " + sec;
+	}, 1000)
+}
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+// STAR RATING function
+function srating() {
+	if (move >= 12 && move < 15) {
+		starCount = 2;
+		starSection[2].style.display = "none";
+	}
+	if (move >= 15) {
+		starCount = 1;
+		starSection[1].style.display = "none";
+	}
+}
